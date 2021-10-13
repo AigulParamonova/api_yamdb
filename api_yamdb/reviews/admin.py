4fd1fdb4a3efca3f93fe2ba3acabfb1233f4187a
Evgeniya <evgenia@agamova.ru>
2021-10-14 00:14:27 +0300

    Реализован импорт из csv файлов в базу данных

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Title, Category, Genre, User, Review
+
+
+EMPTY_VALUE = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Represents the model Comment in admin interface."""
+    list_display = ('id', 'text', 'author', 'review')
+    empty_value_display = EMPTY_VALUE
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Represents the model Category in admin interface."""
+    list_display = ('id', 'name', 'slug')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    """Represents the model Title in admin interface."""
+    list_display = ('id', 'name', 'year', 'category', 'get_genres', 'rating_avg')
+
+    def get_genres(self, obj):
+        return '\n'.join([str(p) for p in obj.genre.all()])
+
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Represents the model Review in admin interface."""
+    list_display = ('id', 'text', 'score', 'author', 'title')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Represents the model User in admin interface."""
+    list_display = ('id', 'username', 'email', 'password')
+    empty_value_display = EMPTY_VALUE
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Represents the model Comment in admin interface."""
+    list_display = ('id', 'name')
+    empty_value_display = EMPTY_VALUE
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,193 @@
-from django.contrib.auth.models import AbstractUser
+import datetime as dt
+import secrets
+import string
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models.aggregates import Avg
+
+now = dt.datetime.now()
+
+
+ROLE_CHOICES = (
+    ('admin', 'admin'),
+    ('moderator', 'moderator'),
+    ('user', 'user'),
+)
+
+
+def create_token():
+    alphabet = string.ascii_letters.upper() + string.digits
+    return ''.join(secrets.choice(alphabet) for i in range(20))
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **kwargs):
+        user = self.model(username=username, email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password, **kwargs):
+        user = self.model(
+            username=username,
+            email=email,
+            is_staff=True,
+            is_superuser=True,
+            **kwargs
+        )
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     bio = models.TextField(
         'Биография',
         blank=True,
     )
+    role = models.CharField(
+        'Пользовательская роль',
+        max_length=10,
+        choices=ROLE_CHOICES,
+    )
+    confirmation_code = models.CharField(
+        'Код подтверждения',
+        max_length=20,
+        default=create_token
+    )
+    REQUIRED_FIELDS = ('email',)
+
+    def is_moderator(self):
+        return self.role == 'moderator'
+
+    def is_admin(self):
+        return self.role == 'admin' or self.is_staff
+
+    objects = CustomUserManager()
+
+
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=256)
+    slug = models.SlugField('Слаг', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=256)
+    slug = models.SlugField('Слаг', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=200)
+    year = models.IntegerField(
+        'Год выпуска',
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(int(now.year))
+        ]
+    )
+    description = models.TextField('Описание', blank=True, null=True)
+    genre = models.ManyToManyField(Genre, 'Жанр', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def rating_avg(self):
+        return self.reviews.aggregate(Avg('score'))
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+    score = models.PositiveSmallIntegerField(
+        'Рейтинг',
+        default=5,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = (
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_review'
+            ),
+        )
+
+    def __str__(self):
+        return f'{self.author}: {self.text[:20]}'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField('Текст')
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'{self.author}: {self.text[:20]}'
