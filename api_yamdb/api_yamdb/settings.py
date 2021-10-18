af831673cec703df5b1740429a31611f0b28acb8
AigulParamonova <aigul.paramonova@yandex.ru>
2021-10-18 15:55:26 +0300

    Поменяли настройки модуля dotenv

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,11 +9,11 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default=None)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', default=None)
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = json.loads(os.getenv('ALLOWED_HOSTS', default=None))
+ALLOWED_HOSTS = json.loads(os.getenv('ALLOWED_HOSTS'))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
